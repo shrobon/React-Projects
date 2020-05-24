26f802442c33d276fbafd7276a4d39e032da4ce7
Shrobon Biswas <shrobon@ualberta.ca>
2020-05-24 18:49:01 -0400

    React State Management part 1

--- indecision-app/README.md
+++ indecision-app/README.md
@@ -16,3 +16,13 @@ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch &
 ### Reading material
 - DOM elements (All supported HTML attributes)
 - React DOM events 
+
+
+### Notes 
+#### React Component State 
+State is an object and is made up of key-value pairs
+Step 1 - Setup the default state object
+Step 2 - Component is rendered with default state values
+Step 3 - Change state based on an event
+Step 4 - Component re-rendered using new state values
+Step 5 - Start again at Step 3
--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,33 +1,83 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+
+
+// // Manual Data Binding
+// const renderCounterApp = () => {
+//     const template2 = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={(addOne)}>addOne</button>
+//             <button onClick={(minusOne)}>minusOne</button>
+//             <button onClick={(reset)}>reset</button>
+
+//         </div>
+//     )
+
+//     ReactDOM.render(template2,appRoot);
+// }
+
+// renderCounterApp();
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleAddOne() {
+        this.setState((prevState)=> {
+            // Only provide the values of the state that you want to update
+            return {
+                count: prevState.count + 1
+            }
+        });
+    }
+
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        })
+    }
+
+    handleReset() {
+        this.setState((prevState) => {
+            return {
+                count: 0
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        )
+    }
 }
 
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
-
-
-// Manual Data Binding
-const renderCounterApp = () => {
-    const template2 = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={(addOne)}>addOne</button>
-            <button onClick={(minusOne)}>minusOne</button>
-            <button onClick={(reset)}>reset</button>
-
-        </div>
-    )
-
-    ReactDOM.render(template2,appRoot);
-}
-
-renderCounterApp();
+ReactDOM.render(<Counter/>, document.getElementById('app'));
